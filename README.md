ef080a0c5a41464c709e915736332a2ae559ddb3
jonghoon <jhwgnu@hanmail.net>
2017-04-18 14:57:11 +0900

    README에 인강 업데이트

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ npm install<br>
 npm start<br>
 
 리액트 설치했으면 소스 폴더 다운받아서 돌리기만 하면 됨
+
+>**Learn Redux from its creator:**  
+>**[Part 1: Getting Started with Redux](https://egghead.io/series/getting-started-with-redux) (30 free videos)**<br>
+>**[Part 2: Building React Applications with Idiomatic Redux](https://egghead.io/courses/building-react-applications-with-idiomatic-redux) (27 free videos)**
--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -2,19 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import Counter from './components/Counter'
-import counter from './reducers'
+import actions from './reducers'
+//actions가 아니라 아무 이름이나 들어가도 상관 없음
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+//reducers의 액션들을 받아들이는 store 생성
+const store = createStore(actions)
 
+//Counter 객체 생성해서 띄워줌
 const render = () => ReactDOM.render(
   <Counter
     value={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    //dispatch(action)을 통해 state을 수정할 수 있음
   />,
-  rootEl
+  document.getElementById('root')
 )
 
 render()
 store.subscribe(render)
+//이 render를 통해 만들어진 컴포넌트를 store의 listener가 되도록 함
